cb18d0e4cc2092d32c3c8e4c9855e5045806a947
Desarrollos Empresariales la Rioja <Christian@MacBook.local>
2012-08-05 21:39:48 +0200

    Commit local desde XCode

--- Ej1T15/ViewController.m
+++ Ej1T15/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"Hola mundo");
+    NSLog(@"Hola mundo cambiado!");
 }
 
 - (void)viewDidUnload
