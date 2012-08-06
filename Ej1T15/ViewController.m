48948b125af3aaf26e1032c596292e955ef7e111
Rubén <ruben.montero@delarioja.net>
2012-08-06 12:33:53 +0200

    Cambio usuario commit

--- Ej1T15/ViewController.m
+++ Ej1T15/ViewController.m
@@ -18,7 +18,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"Hola mundo!! Cambiado usuario");
+    NSLog(@"Hola mundo!! Cambiado nombre usuario");
 }
 
 - (void)viewDidUnload
